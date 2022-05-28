89a886808ddfae79f29f7dc339cffa9dc63e92bc
Sokolov Max <maxerdan@yandex.ru>
2022-05-28 08:09:47 +0500

    4 and 5

--- src/main/java/ru/javastudy/hibernate/main/AppMain.java
+++ src/main/java/ru/javastudy/hibernate/main/AppMain.java
@@ -1,7 +1,11 @@
 package ru.javastudy.hibernate.main;
 
 import com.sun.prism.impl.Disposer;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.LogicalExpression;
+import org.hibernate.criterion.Restrictions;
 import ru.javastudy.hibernate.dao.Person;
 import ru.javastudy.hibernate.dao.RecordBook;
 import ru.javastudy.hibernate.dao.Student;
@@ -9,6 +13,9 @@ import ru.javastudy.hibernate.utils.HibernateSessionFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceUnit;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 /**
@@ -72,6 +79,27 @@ public class AppMain {
             session.save(student);
         }
 
+        List<Person> personsWithA = getAllPersonsWithA(session);
+        listPersons(personsWithA);
+
+        Criteria cr = session.createCriteria(Person.class);
+        Criterion firstName = Restrictions.like("firstName", "%а%");
+        Criterion lastName = Restrictions.like("lastName", "%а%");
+        Criterion middleName = Restrictions.like("middleName", "%а%");
+        LogicalExpression orExp1 = Restrictions.or(firstName,lastName);
+        LogicalExpression orExp2 = Restrictions.or(orExp1,middleName);
+        cr.add( orExp2 );
+        List<Person> personsWithACriteria = cr.list();
+        listPersons(personsWithACriteria);
+
+        List<Student> students = getAllStudentsWithoutRecordBook(session);
+        listStudents(students);
+
+        Criteria cr2 = session.createCriteria(Student.class);
+        cr2.add( Restrictions.isNull("recordBook") );
+        List<Student> studentsWithoutRecordBook = cr2.list();
+        listStudents(studentsWithoutRecordBook);
+
         session.getTransaction().commit();
         session.close();
     }
@@ -94,5 +122,29 @@ public class AppMain {
 
     public static List<Person> getAllPersons(Session session) {
         return session.createQuery("SELECT a FROM Person a").list();
+    }
+
+    public static List<Person> getAllPersonsWithA(Session session) {
+        return session.createQuery("select a from Person a where first_name like '%а%' or last_name like '%а%' or middle_name like '%а%'").list();
+    }
+
+    public static List<Student> getAllStudentsWithoutRecordBook(Session session) {
+        return session.createQuery("select a from Student a where recordBook is null").list();
+    }
+
+    private static void listPersons(List<Person> persons) {
+        System.out.println("ID\tFirstName\tLastName\tMiddleName\tPassportSeria\tPassportNumber");
+        for (Person person : persons) {
+            System.out.println(person.getId() + "\t" + person.getFirstName() + "\t" + person.getLastName() + "\t" + person.getMiddleName() + "\t" + person.getPassportSeria() + "\t" + person.getPassportNumber());
+        }
+        System.out.println("-------------------------");
+    }
+
+    private static void listStudents(List<Student> students) {
+        System.out.println("ID\tGroup\tFirstName\tLastName\tRecordBook");
+        for (Student st : students) {
+            System.out.println(st.getId() + "\t" + st.getGroup() + "\t" + st.getPerson().getFirstName() + "\t" + st.getPerson().getLastName() + "\t" + st.getRecordBook());
+        }
+        System.out.println("-------------------------");
     }
 }
